7719c9c52154bca89270cb8b0ffdef9b1bc49b1f
Areeba1718 <areebasaghir157@gmail.com>
2024-10-15 14:35:37 +0500

    Added free block function

--- bitblockbalancer.cpp
+++ bitblockbalancer.cpp
@@ -3,52 +3,33 @@ using namespace std;
 
 class BitBlockBalancer {
 private:
-    bool* memoryBlocks;  // Dynamically allocated array of memory blocks
+    bool* memoryBlocks;  
     int totalBlocks;
 
 public:
-    // Constructor to initialize memory manager with total blocks
     BitBlockBalancer(int totalBlocks) : totalBlocks(totalBlocks) {
-        memoryBlocks = new bool[totalBlocks];  // Dynamically allocate the array
-        for (int i = 0; i < totalBlocks; i++) {
-            memoryBlocks[i] = false;  // Initialize all blocks to be free
+        memoryBlocks = new bool[totalBlocks]; 
+        for (int i = 0; i < totalBlocks; i++)
+        {
+            memoryBlocks[i] = false;  
         }
     }
-
-    // Destructor to free dynamically allocated memory
     ~BitBlockBalancer() {
         delete[] memoryBlocks;
     }
-
-    // Function to allocate memory block using first-fit strategy
     int allocateBlock() {
         for (int i = 0; i < totalBlocks; i++) {
             if (!memoryBlocks[i]) {
-                memoryBlocks[i] = true;  // Mark block as allocated
+                memoryBlocks[i] = true;  
                 cout << "Allocated block at index: " << i << endl;
-                return i;  // Return the index of the allocated block
+                return i;
             }
         }
         cout << "Error: No free blocks available!" << endl;
-        return -1;  // Allocation failed
+        return -1; 
     }
 
-    // Function to free memory block
-    void freeBlock(int index) {
-        if (index < 0 || index >= totalBlocks) {
-            cout << "Error: Invalid block index!" << endl;
-            return;
-        }
-        if (!memoryBlocks[index]) {
-            cout << "Error: Block at index " << index << " is already free!" << endl;
-        }
-        else {
-            memoryBlocks[index] = false;  // Mark block as free
-            cout << "Freed block at index: " << index << endl;
-        }
-    }
-
-    // Function to display the current state of memory blocks
+    
     void displayMemory() {
         cout << "Memory Blocks (0 = free, 1 = allocated): " << endl;
         for (int i = 0; i < totalBlocks; i++) {
@@ -57,22 +38,23 @@ public:
         cout << endl;
     }
 
-    // Function to defragment memory by moving allocated blocks together
-    void defragmentMemory() {
+    void defragmentMemory() 
+    {
         int writePointer = 0;
-        for (int readPointer = 0; readPointer < totalBlocks; ++readPointer) {
-            if (memoryBlocks[readPointer]) {
-                if (readPointer != writePointer) {
-                    memoryBlocks[writePointer] = true;   // Move allocated block forward
-                    memoryBlocks[readPointer] = false;   // Mark the old spot as free
+        for (int readPointer = 0; readPointer < totalBlocks; ++readPointer) 
+        {
+            if (memoryBlocks[readPointer]) 
+            {
+                if (readPointer != writePointer) 
+                {
+                    memoryBlocks[writePointer] = true;   
+                    memoryBlocks[readPointer] = false;  
                 }
                 ++writePointer;
             }
         }
         cout << "Memory defragmented." << endl;
     }
-
-    // Function to check the number of free blocks
     int countFreeBlocks() {
         int freeBlocks = 0;
         for (int i = 0; i < totalBlocks; i++) {
@@ -84,37 +66,18 @@ public:
     }
 };
 
-int main() {
-    // Initialize BitBlockBalancer with 10 memory blocks
+int main()
+{
     BitBlockBalancer memoryManager(10);
-
-    // Display initial state of memory
     memoryManager.displayMemory();
-
-    // Allocate a few memory blocks
     memoryManager.allocateBlock();
     memoryManager.allocateBlock();
     memoryManager.allocateBlock();
-
-    // Display state of memory after allocation
     memoryManager.displayMemory();
-
-    // Free one block
-    memoryManager.freeBlock(1);
-
-    // Display state of memory after freeing
     memoryManager.displayMemory();
-
-    // Allocate another block
     memoryManager.allocateBlock();
-
-    // Defragment the memory
     memoryManager.defragmentMemory();
-
-    // Display memory after defragmentation
-    memoryManager.displayMemory();
-
-    // Check the number of free blocks
+    memoryManager.displayMemory(); 
     cout << "Free Blocks: " << memoryManager.countFreeBlocks() << endl;
 
     return 0;
